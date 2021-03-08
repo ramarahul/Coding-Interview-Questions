4d7bf841f139093e43709b7ece371b0b98843184
Venkata Rama Rahul Chintalapati <ramarahulchintalapati@gmail.com>
2021-03-08 22:55:47 +0530

    Create Gas Station(LeetCode-134).java

---
+++ Gas Station(LeetCode-134).java
@@ -0,0 +1,16 @@
+class Solution{
+  public int canCompleteCircuit(int[] gas, int[] cost){
+    int tank = 0, total = 0, index = 0;
+    for(int i = 0; i < gas.length ; i++){
+      int consume = gas[i] - cost[i];
+      tank += consume;
+      if(tank < 0){
+        index = i + 1;
+        tank = 0;
+      }
+      total += consume;
+    }
+    
+    return total < 0 ? -1 : index;
+  }
+}
