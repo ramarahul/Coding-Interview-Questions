fea745e1c2db87239a89a5c7a903dc28280f112e
Venkata Rama Rahul Chintalapati <ramarahulchintalapati@gmail.com>
2021-03-09 13:01:37 +0530

    Create Subsets(LeetCode-78).java

---
+++ Subsets(LeetCode-78).java
@@ -0,0 +1,16 @@
+class Solution{
+  public List<List<Integer>> subsets(int[] nums){
+    List<List<Integer>> result = new ArrayList();
+    result.add(new ArrayList());
+    for(int num: nums){
+      int size = result.size();
+      for(int i=0; i<size; i++){
+        List<Integer> subset = new ArrayList<Integer>(result.get(i));
+        subset.add(num);
+        result.add(subset);
+      }
+    }
+    
+    return result;
+  }
+}
