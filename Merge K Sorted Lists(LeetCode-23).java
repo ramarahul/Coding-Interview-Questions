7c0d4d173f104fa827de733477accf6cd24fc4ec
Venkata Rama Rahul Chintalapati <ramarahulchintalapati@gmail.com>
2021-03-14 13:05:03 +0530

    Create Merge K Sorted Lists(LeetCode-23).java

---
+++ Merge K Sorted Lists(LeetCode-23).java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+      PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(new Comparator<ListNode>(){
+        @Override
+        public int compare(LisNode l1, ListNode l2){
+          return l1.val-l2.val;
+        }
+      });
+      
+      ListNode head = new ListNode(0);
+      for(ListNode list : lists){
+        pq.add(list);
+      }
+      
+      ListNode tail = head;
+      while(!pq.isEmpty()){
+        tail.next = pq.poll();
+        tail=tail.next;
+        if(tail.next!=null){
+          pq.add(tail.next);
+        }
+      }
+      
+      return head.next;
+    }
+}
