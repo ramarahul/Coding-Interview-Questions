ea1f189f84a458856dc949f02986758f7f81a48e
Venkata Rama Rahul Chintalapati <ramarahulchintalapati@gmail.com>
2021-03-14 22:10:21 +0530

    Create Validate Binary Search Tree(LeetCode-98).java

---
+++ Validate Binary Search Tree(LeetCode-98).java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if(root==null) return true;
+        
+        return isValidBSTWithRange(root,null,null);
+        
+    }
+    
+    public boolean isValidBSTWithRange(TreeNode root, Integer min, Integer max){
+      if(root==null) return true;
+      if((min!=null && root.val <= min) || (max!=null && root.val >= max))
+        return false;
+      
+      boolean left = isValidBSTWithRange(root.left,min,root.val);
+      boolean right = isValidBSTWithRange(root.right,root.val,max);
+      
+      return (left&&right);
+        
+    }
+}
