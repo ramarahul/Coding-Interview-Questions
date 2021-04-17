f74598fe180c361ba456423357f769f41fec06d6
Venkata Rama Rahul Chintalapati <ramarahulchintalapati@gmail.com>
2021-04-17 15:10:34 +0530

    Update Search a 2D Matrix II(LeetCode-240).java

--- Search a 2D Matrix II(LeetCode-240).java
+++ Search a 2D Matrix II(LeetCode-240).java
@@ -2,15 +2,15 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if(matrix.length==0 || matrix[0].length==0) 
           return false;
-      
+
         int i = 0;
         int j = matrix[0].length -1;
         while(i<matrix.length && j>=0){
           if(matrix[i][j]==target) return true;
-          if(matrix[i][j]<target) i++;
-          if(matrix[i][j]>target) j--;
+          else if(matrix[i][j]<target) i++;
+          else if(matrix[i][j]>target) j--;
         }
-      
+
         return false;
     }
 }
