e3ed00df7ddea75c6e23159d950b36252921b153
Venkata Rama Rahul Chintalapati <ramarahulchintalapati@gmail.com>
2021-06-25 12:30:33 +0530

    Create Median of Two Sorted Arrays(LeetCode-4).java

---
+++ Median of Two Sorted Arrays(LeetCode-4).java
@@ -0,0 +1,44 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+        if(nums1.length > nums2.length){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        
+        int x = nums1.length;
+        int y = nums2.length;
+        
+        int minIndex = 0, maxIndex = x;
+        
+        while(minIndex <= maxIndex){
+            int partitionX = (minIndex+maxIndex)/2;
+            int partitionY = (x+y+1)/2 -partitionX;
+            
+            
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
+            
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
+            
+            if(maxLeftX <= minRightY && maxLeftY <= minRightX){
+                
+                if((x+y)%2==0){
+                    return (double)(Math.max(maxLeftX,maxLeftY)+Math.min(minRightX,minRightY))/2;
+                }
+                else{
+                    return (double)Math.max(maxLeftX,maxLeftY);
+                }
+                
+            }
+            else if(maxLeftX > minRightY){
+                maxIndex = partitionX - 1;
+            }
+            else{
+                minIndex = partitionX + 1;
+            }
+        }
+        
+        return 0.0;
+    }
+}
